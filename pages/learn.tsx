18b13105d7d24cc514e955b1dfec7d3faa507400
awpenheimer_ <awpdevmain@gmail.com>
2023-05-29 19:40:16 -0700

    shortened max card width on learn page

--- pages/learn.tsx
+++ pages/learn.tsx
@@ -1,7 +1,27 @@
 import Link from "next/link";
-
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+
+const learnItems = [
+  {
+    title: "Causes of Food Insecurity",
+    description:
+      "Learn about the root causes of food insecurity, including poverty, climate change, and conflict.",
+    link: "/causes",
+  },
+  {
+    title: "Impact of Food Insecurity",
+    description:
+      "Understand the devastating effects of food insecurity on individuals, communities, and economies.",
+    link: "/impact",
+  },
+  {
+    title: "Solutions to Food Insecurity",
+    description:
+      "Discover possible solutions to combat food insecurity, such as increasing access to education, improving agricultural practices, and supporting food banks.",
+    link: "/solutions",
+  },
+];
 
 export default function Learn() {
   return (
@@ -31,60 +51,27 @@ export default function Learn() {
             better understand this complex issue and take action to combat it.
           </p>
         </div>
-        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
-          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
-            <div className="flex flex-col bg-gray-800 rounded-xl p-6">
-              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
-                Causes of Food Insecurity
-              </dt>
-              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-indigo-200">
-                <p className="flex-auto">
-                  Learn about the root causes of food insecurity, including
-                  poverty, climate change, and conflict.
-                </p>
-                <Link
-                  href="/causes"
-                  className="text-sm font-semibold leading-6 text-sky-300 select-none mt-6 hover:text-sky-100"
-                >
-                  Learn more →
-                </Link>
-              </dd>
-            </div>
-            <div className="flex flex-col bg-gray-800 rounded-xl p-6">
-              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
-                Impact of Food Insecurity
-              </dt>
-              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-indigo-200">
-                <p className="flex-auto">
-                  Understand the devastating effects of food insecurity on
-                  individuals, communities, and economies.
-                </p>
-                <Link
-                  href="/impact"
-                  className="text-sm font-semibold leading-6 text-sky-300 select-none mt-6 hover:text-sky-100"
-                >
-                  Learn more →
-                </Link>
-              </dd>
-            </div>
-            <div className="flex flex-col bg-gray-800 rounded-xl p-6">
-              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
-                Solutions to Food Insecurity
-              </dt>
-              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-indigo-200">
-                <p className="flex-auto">
-                  Discover possible solutions to combat food insecurity, such as
-                  increasing access to education, improving agricultural
-                  practices, and supporting food banks.
-                </p>
-                <Link
-                  href="/solutions"
-                  className="text-sm font-semibold leading-6 text-sky-300 select-none mt-6 hover:text-sky-100"
-                >
-                  Learn more →
-                </Link>
-              </dd>
-            </div>
+        <div className="mx-auto mt-16 max-w-4xl sm:mt-20 lg:mt-24">
+          <dl className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            {learnItems.map((item, index) => (
+              <div
+                key={index}
+                className="flex flex-col bg-gray-800 rounded-xl p-6"
+              >
+                <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
+                  {item.title}
+                </dt>
+                <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-indigo-200">
+                  <p className="flex-auto">{item.description}</p>
+                  <Link
+                    href={item.link}
+                    className="text-sm font-semibold leading-6 text-sky-300 select-none mt-6 hover:text-sky-100"
+                  >
+                    Learn more →
+                  </Link>
+                </dd>
+              </div>
+            ))}
           </dl>
         </div>
       </div>
