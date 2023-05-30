462639dd35cd3c3bc86fbe16361e8bb9a9f915c2
awpenheimer_ <awpdevmain@gmail.com>
2023-05-29 20:53:09 -0700

    added dynamic title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useRouter } from "next/router";
+import Head from "next/head";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  const pageTitles: { [key: string]: string } = {
+    "/": "Food Insecurity - j.s",
+    "/learn": "Learn - j.s",
+    "/causes": "Causes - j.s",
+    "/impact": "Impact - j.s",
+    "/solutions": "Solutions - j.s",
+    "/donate": "Donate - j.s",
+  };
+
+  const pageTitle = pageTitles[router.pathname] || "Food Insecurity - j.s";
+
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
